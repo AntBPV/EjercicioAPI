147ecc1c67811859bd423d25d53c5855a6f3f4be
AntonhyBPV <antonio.parra@campusucc.edu.co>
2024-08-30 12:27:57 -0500

    App Calculadora

---
+++ calculadora/apiCalculadora/views.py
@@ -0,0 +1,47 @@
+import json
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+
+# Create your views here.
+class SumaView(APIView):
+    def get(self,request,*args,**kwargs):
+        num_uno=request.GET.get('num_uno')
+        num_dos=request.GET.get('num_dos')
+        suma=int(num_uno)+int(num_dos)
+        return Response(suma, status=status.HTTP_200_OK)
+    
+class RestaView(APIView):
+    def get(self,request,*args,**kwargs):
+        num_uno=request.GET.get('num_uno')
+        num_dos=request.GET.get('num_dos')
+        resta=int(num_uno)-int(num_dos)
+        return Response(resta, status=status.HTTP_200_OK)
+    
+class MultiplicacionView(APIView):
+    def get(self,request,*args,**kwargs):
+        num_uno=request.GET.get('num_uno')
+        num_dos=request.GET.get('num_dos')
+        multiplicacion=int(num_uno)*int(num_dos)
+        return Response(multiplicacion, status=status.HTTP_200_OK)
+    
+class DivisionView(APIView):
+    def get(self,request,*args,**kwargs):
+        num_uno=request.GET.get('num_uno')
+        num_dos=request.GET.get('num_dos')
+        
+        try:
+            num_uno = int(num_uno)
+            num_dos = int(num_dos)
+
+            if num_dos == 0:
+                return Response("No se puede dividir por cero", status=status.HTTP_400_BAD_REQUEST)
+
+            division = num_uno / num_dos
+            return Response(division, status=status.HTTP_200_OK)
+
+        except ValueError:
+            return Response("Los números deben ser enteros válidos", status=status.HTTP_400_BAD_REQUEST)
+
+        
